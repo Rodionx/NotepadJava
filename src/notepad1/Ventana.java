8a16172c7ef89add8ed349f132dc07f1866ad5cf
xp <xp@CETYSDAM00>
2021-10-21 13:36:22 +0200

    Notepad terminado.

--- src/notepad1/Ventana.java
+++ src/notepad1/Ventana.java
@@ -5,6 +5,7 @@
  */
 package notepad1;
 
+import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.File;
@@ -15,6 +16,8 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFileChooser;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.UndoManager;
 
 /**
  *
@@ -22,22 +25,31 @@ import javax.swing.JFileChooser;
  */
 public class Ventana extends javax.swing.JFrame {
 
+    
+    String archivoAbierto=""; 
+    
     public Ventana() {
         initComponents();
+        jTextArea1.getDocument().addUndoableEditListener(manager);
     }
+    
+    private UndoManager manager = new UndoManager();
     
     public void abrirArchivo() {
     JFileChooser fileChooser = new JFileChooser();
     fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-    if (JFileChooser.APPROVE_OPTION == fileChooser.showOpenDialog(jMenu3)) {
+    if (JFileChooser.APPROVE_OPTION == fileChooser.showOpenDialog(itemNuevo)) {
         File archivo = fileChooser.getSelectedFile();
         FileReader lector = null;
+        this.setTitle(archivo.getName());
         try {
             lector = new FileReader(archivo);
             BufferedReader bfReader = new BufferedReader(lector);
 
             String lineaFichero;
             StringBuilder contenidoFichero = new StringBuilder();
+            
+            archivoAbierto = archivo.getAbsolutePath();
 
             // Recupera el contenido del fichero
             while ((lineaFichero = bfReader.readLine()) != null) {
@@ -47,7 +59,7 @@ public class Ventana extends javax.swing.JFrame {
 
             // Pone el contenido del fichero en el area de texto
             jTextArea1.setText(contenidoFichero.toString());
-
+            
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Ventana.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
@@ -63,26 +75,52 @@ public class Ventana extends javax.swing.JFrame {
 }
     
 public void guardarArchivo() {
-    JFileChooser fileChooser = new JFileChooser();
-    fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-    if (JFileChooser.APPROVE_OPTION == fileChooser.showSaveDialog(jMenu3)) {
-        File archivo = fileChooser.getSelectedFile();
-        FileWriter escritor = null;
-        try {
-            escritor = new FileWriter(archivo);
-            escritor.write(jTextArea1.getText());
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(Ventana.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(Ventana.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        if (JFileChooser.APPROVE_OPTION == fileChooser.showSaveDialog(itemNuevo)) {
+            File archivo = fileChooser.getSelectedFile();
+            FileWriter escritor = null;
             try {
-                escritor.close();
+                escritor = new FileWriter(archivo);
+                escritor.write(jTextArea1.getText());
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(Ventana.class.getName()).log(Level.SEVERE, null, ex);
             } catch (IOException ex) {
                 Logger.getLogger(Ventana.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                try {
+                    escritor.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(Ventana.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         }
-    }
+}
+
+public void guardar() {
+    FileWriter escritor = null;
+       try {
+                escritor = new FileWriter(archivoAbierto);
+                escritor.write(jTextArea1.getText());
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(Ventana.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(Ventana.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                try {
+                    escritor.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(Ventana.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+}
+
+public void deshacer() {
+    try {
+          manager.undo();
+        } catch (CannotUndoException ex){
+            ex.printStackTrace();
+        }
 }
     
     @SuppressWarnings("unchecked")
@@ -91,57 +129,182 @@ public void guardarArchivo() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
-        jMenuBar2 = new javax.swing.JMenuBar();
-        jMenu3 = new javax.swing.JMenu();
-        jMenu4 = new javax.swing.JMenu();
-        jMenu5 = new javax.swing.JMenu();
-        jMenu6 = new javax.swing.JMenu();
+        Menu = new javax.swing.JMenuBar();
+        menuArchivo = new javax.swing.JMenu();
+        itemNuevo = new javax.swing.JMenuItem();
+        itemAbrir = new javax.swing.JMenuItem();
+        itemGuardar = new javax.swing.JMenuItem();
+        itemGuardarComo = new javax.swing.JMenuItem();
+        itemSalir = new javax.swing.JMenuItem();
+        menuEdicion = new javax.swing.JMenu();
+        jMenuItem6 = new javax.swing.JMenuItem();
+        jMenuItem7 = new javax.swing.JMenuItem();
+        jMenuItem8 = new javax.swing.JMenuItem();
+        jMenuItem9 = new javax.swing.JMenuItem();
+        jMenuItem10 = new javax.swing.JMenuItem();
+        jMenuItem11 = new javax.swing.JMenuItem();
+        jMenuItem12 = new javax.swing.JMenuItem();
+        jMenuItem13 = new javax.swing.JMenuItem();
+        jMenuItem14 = new javax.swing.JMenuItem();
+        jMenuItem15 = new javax.swing.JMenuItem();
+        jMenuItem16 = new javax.swing.JMenuItem();
+        menuFuente = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
+        jMenuItem4 = new javax.swing.JMenuItem();
+        jMenuItem5 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Bloc de Notas");
 
         jTextArea1.setColumns(20);
         jTextArea1.setRows(5);
         jScrollPane1.setViewportView(jTextArea1);
 
-        jMenuBar2.setName("Menu");
+        Menu.setName("Menu"); // NOI18N
 
-        jMenu3.setText("Nuevo");
-        jMenu3.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
-        jMenu3.setName("Nuevo"); // NOI18N
-        jMenu3.addMouseListener(new java.awt.event.MouseAdapter() {
+        menuArchivo.setText("Archivo");
+        menuArchivo.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jMenu3MouseClicked(evt);
+                menuArchivoMouseClicked(evt);
             }
         });
-        jMenu3.addActionListener(new java.awt.event.ActionListener() {
+
+        itemNuevo.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_N, java.awt.event.InputEvent.CTRL_MASK));
+        itemNuevo.setText("Nuevo");
+        itemNuevo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenu3ActionPerformed(evt);
+                itemNuevoActionPerformed(evt);
             }
         });
-        jMenuBar2.add(jMenu3);
+        menuArchivo.add(itemNuevo);
 
-        jMenu4.setText("Abrir...");
-        jMenu4.setName("Abrir"); // NOI18N
-        jMenu4.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jMenu4MouseClicked(evt);
+        itemAbrir.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_A, java.awt.event.InputEvent.CTRL_MASK));
+        itemAbrir.setText("Abrir...");
+        itemAbrir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                itemAbrirActionPerformed(evt);
             }
         });
-        jMenuBar2.add(jMenu4);
+        menuArchivo.add(itemAbrir);
 
-        jMenu5.setText("Guardar");
-        jMenu5.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jMenu5MouseClicked(evt);
+        itemGuardar.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_G, java.awt.event.InputEvent.CTRL_MASK));
+        itemGuardar.setText("Guardar");
+        itemGuardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                itemGuardarActionPerformed(evt);
             }
         });
-        jMenuBar2.add(jMenu5);
+        menuArchivo.add(itemGuardar);
 
-        jMenu6.setText("Fuente");
-        jMenuBar2.add(jMenu6);
+        itemGuardarComo.setText("Guardar como...");
+        itemGuardarComo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                itemGuardarComoActionPerformed(evt);
+            }
+        });
+        menuArchivo.add(itemGuardarComo);
 
-        setJMenuBar(jMenuBar2);
-        jMenuBar2.getAccessibleContext().setAccessibleName("Menu");
+        itemSalir.setText("Salir");
+        itemSalir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                itemSalirActionPerformed(evt);
+            }
+        });
+        menuArchivo.add(itemSalir);
+
+        Menu.add(menuArchivo);
+
+        menuEdicion.setText("Edicion");
+
+        jMenuItem6.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_Z, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuItem6.setText("Deshacer");
+        jMenuItem6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem6ActionPerformed(evt);
+            }
+        });
+        menuEdicion.add(jMenuItem6);
+
+        jMenuItem7.setText("jMenuItem6");
+        menuEdicion.add(jMenuItem7);
+
+        jMenuItem8.setText("jMenuItem6");
+        menuEdicion.add(jMenuItem8);
+
+        jMenuItem9.setText("jMenuItem6");
+        menuEdicion.add(jMenuItem9);
+
+        jMenuItem10.setText("jMenuItem6");
+        menuEdicion.add(jMenuItem10);
+
+        jMenuItem11.setText("jMenuItem6");
+        menuEdicion.add(jMenuItem11);
+
+        jMenuItem12.setText("jMenuItem6");
+        menuEdicion.add(jMenuItem12);
+
+        jMenuItem13.setText("jMenuItem6");
+        menuEdicion.add(jMenuItem13);
+
+        jMenuItem14.setText("jMenuItem6");
+        menuEdicion.add(jMenuItem14);
+
+        jMenuItem15.setText("jMenuItem6");
+        menuEdicion.add(jMenuItem15);
+
+        jMenuItem16.setText("jMenuItem6");
+        menuEdicion.add(jMenuItem16);
+
+        Menu.add(menuEdicion);
+
+        menuFuente.setText("Fuente");
+
+        jMenuItem1.setText("Arial");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        menuFuente.add(jMenuItem1);
+
+        jMenuItem2.setText("Bauhaus93");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
+        menuFuente.add(jMenuItem2);
+
+        jMenuItem3.setText("Cooper Black");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
+        menuFuente.add(jMenuItem3);
+
+        jMenuItem4.setText("Vladimir Script");
+        jMenuItem4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem4ActionPerformed(evt);
+            }
+        });
+        menuFuente.add(jMenuItem4);
+
+        jMenuItem5.setText("Onyx");
+        jMenuItem5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem5ActionPerformed(evt);
+            }
+        });
+        menuFuente.add(jMenuItem5);
+
+        Menu.add(menuFuente);
+
+        setJMenuBar(Menu);
+        Menu.getAccessibleContext().setAccessibleName("Menu");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -157,21 +320,58 @@ public void guardarArchivo() {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jMenu3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenu3ActionPerformed
-        
-    }//GEN-LAST:event_jMenu3ActionPerformed
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        jTextArea1.setFont(new Font("Arial", Font.PLAIN, 14));
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
-    private void jMenu3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenu3MouseClicked
-       jTextArea1.setText("");
-    }//GEN-LAST:event_jMenu3MouseClicked
+    private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
+        jTextArea1.setFont(new Font("Vladimir Script", Font.PLAIN, 14));
+    }//GEN-LAST:event_jMenuItem4ActionPerformed
 
-    private void jMenu4MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenu4MouseClicked
+    private void jMenuItem5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem5ActionPerformed
+        jTextArea1.setFont(new Font("Onyx", Font.PLAIN, 14));
+    }//GEN-LAST:event_jMenuItem5ActionPerformed
+
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        jTextArea1.setFont(new Font("Bauhaus 93", Font.PLAIN, 14));
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        jTextArea1.setFont(new Font("Cooper Black", Font.PLAIN, 14));
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
+
+    private void itemNuevoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itemNuevoActionPerformed
+        jTextArea1.setText("");
+    }//GEN-LAST:event_itemNuevoActionPerformed
+
+    private void itemGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itemGuardarActionPerformed
+        if(archivoAbierto == ""){
+            guardarArchivo();
+        }
+        else{
+            guardar();
+        }
+    }//GEN-LAST:event_itemGuardarActionPerformed
+
+    private void itemSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itemSalirActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_itemSalirActionPerformed
+
+    private void itemAbrirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itemAbrirActionPerformed
         abrirArchivo();
-    }//GEN-LAST:event_jMenu4MouseClicked
+    }//GEN-LAST:event_itemAbrirActionPerformed
 
-    private void jMenu5MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenu5MouseClicked
+    private void menuArchivoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_menuArchivoMouseClicked
+       
+    }//GEN-LAST:event_menuArchivoMouseClicked
+
+    private void itemGuardarComoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itemGuardarComoActionPerformed
         guardarArchivo();
-    }//GEN-LAST:event_jMenu5MouseClicked
+    }//GEN-LAST:event_itemGuardarComoActionPerformed
+
+    private void jMenuItem6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem6ActionPerformed
+        deshacer();
+    }//GEN-LAST:event_jMenuItem6ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -209,12 +409,36 @@ public void guardarArchivo() {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenu jMenu3;
-    private javax.swing.JMenu jMenu4;
-    private javax.swing.JMenu jMenu5;
-    private javax.swing.JMenu jMenu6;
-    private javax.swing.JMenuBar jMenuBar2;
+    private javax.swing.JMenuBar Menu;
+    private javax.swing.JMenuItem itemAbrir;
+    private javax.swing.JMenuItem itemGuardar;
+    private javax.swing.JMenuItem itemGuardarComo;
+    private javax.swing.JMenuItem itemNuevo;
+    private javax.swing.JMenuItem itemSalir;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem10;
+    private javax.swing.JMenuItem jMenuItem11;
+    private javax.swing.JMenuItem jMenuItem12;
+    private javax.swing.JMenuItem jMenuItem13;
+    private javax.swing.JMenuItem jMenuItem14;
+    private javax.swing.JMenuItem jMenuItem15;
+    private javax.swing.JMenuItem jMenuItem16;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem jMenuItem5;
+    private javax.swing.JMenuItem jMenuItem6;
+    private javax.swing.JMenuItem jMenuItem7;
+    private javax.swing.JMenuItem jMenuItem8;
+    private javax.swing.JMenuItem jMenuItem9;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JMenu menuArchivo;
+    private javax.swing.JMenu menuEdicion;
+    private javax.swing.JMenu menuFuente;
     // End of variables declaration//GEN-END:variables
+
+    private void setTitle(File archivo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
